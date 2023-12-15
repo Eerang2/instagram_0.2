8cd6168286a6e236ee7bfcafee4e7a0f955dc5c8
이어진 <ejlee@hanatour.com>
2023-12-15 12:34:21 +0900

    [ejlee] 테스트에 사용하지않는 주입 제거

--- src/test/java/com/instagram_02/board/service/PostServiceTest.java
+++ src/test/java/com/instagram_02/board/service/PostServiceTest.java
@@ -1,18 +1,15 @@
 package com.instagram_02.board.service;
 
 import com.instagram_02.board.entity.Post;
-import com.instagram_02.board.repository.PostRepository;
-import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class PostServiceTest {
@@ -20,14 +17,9 @@ class PostServiceTest {
     @Autowired
     private PostService postService;
 
-    @Autowired
-    EntityManager em;
-    @Autowired
-    private PostRepository postRepository;
-
     @Test
     @DisplayName("게시판_전제조회_테스트")
-    public void 게시판_전제조회_테스트 () {
+    public void 게시판_전체조회_테스트() {
         // given
         Post post = Post.builder()
                 .id(1L)
@@ -35,7 +27,6 @@ class PostServiceTest {
                 .content("챔스 조별 4위")
                 .userId("Erik ten Hag")
                 .build();
-
 
         postService.save(post);
 
